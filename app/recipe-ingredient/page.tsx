9d13475b8953c2132db9f81de9d78bfdd24e4360
WinstonAlfred <winstonalfred48@gmail.com>
2025-06-30 19:49:15 +0700

    new file:   app/recipe-ingredient/create/page.tsx 	new file:   app/recipe-ingredient/page.tsx 	modified:   components/createForm/createRecipeIngredientForm.tsx 	modified:   components/navbar.tsx 	new file:   components/table/recipeIngredientsTable.tsx 	new file:   lib/actions/recipeIngredientsAction.tsx 	new file:   lib/data/getRecipeIngredient.ts

---
+++ app/recipe-ingredient/page.tsx
@@ -0,0 +1,12 @@
+import RecipeIngredientTable from '@/components/table/recipeIngredientsTable'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <RecipeIngredientTable />
+    </div>
+  )
+}
+
+export default page
