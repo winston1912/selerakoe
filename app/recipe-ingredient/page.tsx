9acd7132ce8256b3b989578d05b208ab32a65ece
WinstonAlfred <winstonalfred48@gmail.com>
2025-07-04 16:54:25 +0700

    modified:   app/recipe-ingredient/page.tsx 	modified:   app/recipes/page.tsx

--- app/recipe-ingredient/page.tsx
+++ app/recipe-ingredient/page.tsx
@@ -1,6 +1,8 @@
 import RecipeIngredientTable from '@/components/table/recipeIngredientsTable'
 import React from 'react'
 
+export const dynamic = 'force-dynamic';
+
 const page = () => {
   return (
     <div>
--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,6 +1,8 @@
 import RecipeTable from '@/components/table/recipeTable'
 import React from 'react'
 
+export const dynamic = 'force-dynamic';
+
 const page = () => {
   return (
     <div>
